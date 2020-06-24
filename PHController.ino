87f9625b283e13de3839305d2a5e923372a1f172
BlazingWizard <lobanov.viktor.v@gmail.com>
2020-06-24 19:57:37 +0300

    Move work with LCD to separate file

--- PHController.ino
+++ PHController.ino
@@ -3,52 +3,15 @@
 #include <GyverEncoder.h>
 
 #include "Menu.h"
+#include "CreateMenu.h"
 
-LiquidCrystal_I2C lcd(0x3F, 20, 4);
 Encoder enc1(2, 3, 4, TYPE2);
-
-Menu *menu = new Menu();
-
-void onShowMain(){
-  lcd.clear();
-  lcd.printstr("Main Screen");
-}
-
-void onShowSettings(){
-  lcd.clear();
-  lcd.printstr("Settings");
-}
-
-void test1(){
-  lcd.clear();
-  lcd.printstr("test1");
-}
-
-void test2(){
-  lcd.clear();
-  lcd.printstr("test2");
-}
-
-void test3(){
-  lcd.clear();
-  lcd.printstr("test3");
-}
+Menu *menu = NULL;
 
 void setup() {
   Serial.begin(38400);
-  lcd.init();
-  lcd.backlight();
-  
-  MenuElement *ms = new MenuElement(onShowMain);
-  MenuElement *s = new MenuElement(onShowSettings);
-
-  ms->subMenu = new Menu();
-  ms->subMenu->add(new MenuElement(test1));
-  ms->subMenu->add(new MenuElement(test2));
-  ms->subMenu->add(new MenuElement(test3));
-
-  menu->add(ms);
-  menu->add(s);
+  initLCD();
+  menu = createMenu();
 }
 
 void loop() {
