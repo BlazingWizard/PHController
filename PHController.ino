f1b45994a6b2b2fc914cd638db5515069bdf3c95
BlazingWizard <lobanov.viktor.v@gmail.com>
2020-06-26 19:51:09 +0300

    Add template for menu

--- PHController.ino
+++ PHController.ino
@@ -6,23 +6,13 @@
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 Encoder enc1(2, 3, 4, TYPE2);
-Menu *menu = new Menu();
+Menu *menu = NULL;
 
 void setup() {
   Serial.begin(38400);
   lcd.init();
   lcd.backlight();
-  
-  MenuElement *ms = new MenuElement(onShowMain);
-  MenuElement *s = new MenuElement(onShowSettings);
-
-  ms->subMenu = new Menu();
-  ms->subMenu->add(new MenuElement(test1));
-  ms->subMenu->add(new MenuElement(test2));
-  ms->subMenu->add(new MenuElement(test3));
-
-  menu->add(ms);
-  menu->add(s);
+  menu = createMenu();
 }
 
 void loop() {
@@ -43,7 +33,7 @@ void loop() {
   menu->callOnShowAction();
 }
 
-
+#pragma region MainMenu
 void onShowMain(){
   lcd.clear();
   lcd.printstr("Main Screen");
@@ -54,17 +44,127 @@ void onShowSettings(){
   lcd.printstr("Settings");
 }
 
-void test1(){
+void onShowPhCalibration() {
   lcd.clear();
-  lcd.printstr("test1");
+  lcd.printstr("Ph Calibration");
 }
 
-void test2(){
+void onShowTestPump(){
   lcd.clear();
-  lcd.printstr("test2");
+  lcd.printstr("Test Pump");
+}
+#pragma endregion MainMenu
+
+#pragma region SettingsSubmenu
+void onShowSetMode(){
+  lcd.clear();
+  lcd.printstr("Mode");
 }
 
-void test3(){
+void onShowSetPhLow(){
   lcd.clear();
-  lcd.printstr("test3");
+  lcd.printstr("PhLow");
+}
+
+void onShowSetPhHigh(){
+  lcd.clear();
+  lcd.printstr("PhHigh");
+}
+
+void onShowSetSmallAdjust(){
+  lcd.clear();
+  lcd.printstr("Small Adjust");
+}
+
+void onShowSetLargeAdjust(){
+  lcd.clear();
+  lcd.printstr("Large Adjust");
+}
+
+void onShowSetDelay(){
+  lcd.clear();
+  lcd.printstr("Delay");
+}
+
+void onShowSetLED(){
+  lcd.clear();
+  lcd.printstr("LED");
+}
+
+void onShowResetAll(){
+  lcd.clear();
+  lcd.printstr("Reset All");
+}
+#pragma endregion SettingsSubmenu
+
+#pragma region PhCalibrationSubmenu
+void onShowCalibratePh4(){
+  lcd.clear();
+  lcd.printstr("Calibrate Ph4");
+}
+
+void onShowCalibratePh7(){
+  lcd.clear();
+  lcd.printstr("Calibrate Ph7");
+}
+
+void onShowCalibratePh10(){
+  lcd.clear();
+  lcd.printstr("Calibrate Ph10");
+}
+#pragma endregion PhCalibrationSubmenu
+
+#pragma region TestPumpSubmenu
+void onShowPump1(){
+  lcd.clear();
+  lcd.printstr("Pump1");
+}
+
+void onShowPump2(){
+  lcd.clear();
+  lcd.printstr("Pump2");
+}
+#pragma endregion TestPumpSubmenu
+
+Menu* createMenu(){
+  Menu* mainMenu = new Menu();
+  
+  // Create elemnts main menu
+  MenuElement *mainScreen = new MenuElement(onShowMain);
+  MenuElement *settings = new MenuElement(onShowSettings);
+  settings->subMenu = new Menu();
+  MenuElement *phCalibration = new MenuElement(onShowPhCalibration);
+  phCalibration->subMenu = new Menu();
+  MenuElement *testPump = new MenuElement(onShowTestPump);
+  testPump->subMenu = new Menu();
+
+  // Create settings submenu
+  Menu* settingsSubmenu = settings->subMenu;
+  settingsSubmenu->add(new MenuElement(onShowSetMode));
+  settingsSubmenu->add(new MenuElement(onShowSetPhLow));
+  settingsSubmenu->add(new MenuElement(onShowSetPhHigh));
+  settingsSubmenu->add(new MenuElement(onShowSetSmallAdjust));
+  settingsSubmenu->add(new MenuElement(onShowSetLargeAdjust));
+  settingsSubmenu->add(new MenuElement(onShowSetDelay));
+  settingsSubmenu->add(new MenuElement(onShowSetLED));
+  settingsSubmenu->add(new MenuElement(onShowResetAll));
+
+  // Create phCalibration submenu
+  Menu* phCalibrationSubmenu = phCalibration->subMenu;
+  phCalibrationSubmenu->add(new MenuElement(onShowCalibratePh4));
+  phCalibrationSubmenu->add(new MenuElement(onShowCalibratePh7));
+  phCalibrationSubmenu->add(new MenuElement(onShowCalibratePh10));
+
+  // Create testPump submenu
+  Menu* testPumpSubmenu = testPump->subMenu;
+  testPumpSubmenu->add(new MenuElement(onShowPump1));
+  testPumpSubmenu->add(new MenuElement(onShowPump2));
+
+  // Fill main menu
+  mainMenu->add(mainScreen);
+  mainMenu->add(settings);
+  mainMenu->add(phCalibration);
+  mainMenu->add(testPump);
+
+  return mainMenu;
 }
