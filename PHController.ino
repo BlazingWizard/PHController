7f84dbbc93ead7cbd7556128cba78760f152c0b9
BlazingWizard <lobanov.viktor.v@gmail.com>
2020-07-10 23:45:53 +0300

    Add menu functions

--- PHController.ino
+++ PHController.ino
@@ -5,12 +5,14 @@
 #include "Menu.h"
 #include "Settings.h"
 
-LiquidCrystal_I2C lcd(0x3F, 20, 4);
+#define SCREEN_WIDTH 20
+
+LiquidCrystal_I2C lcd(0x3F, SCREEN_WIDTH, 4);
 Encoder enc1(2, 3, 4, TYPE2);
 
 Menu *menu = NULL;
 Settings *settings = NULL;
-char* settingsStr = new char[20];
+char* settingsStr = new char[SCREEN_WIDTH];
 
 void setup() {
   Serial.begin(38400);
@@ -70,6 +72,7 @@ void onShowTestPump(){
 #pragma endregion MainMenu
 
 #pragma region SettingsSubmenu
+#pragma region Mode
 void onShowSetMode(){
   lcd.clear();
   lcd.printstr("Mode");
@@ -80,54 +83,148 @@ void onShowSetMode(){
 }
 
 void onHoldRightMode(){
-  int mode = settings->mode->getValue();
-  mode++;
-  settings->mode->setValue(mode);
+  settings->mode->inc();
   onShowSetMode();
 }
 
 void onHoldLeftMode(){
-  int mode = settings->mode->getValue();
-  mode--;
-  settings->mode->setValue(mode);
+  settings->mode->dec();
   onShowSetMode();
 }
+#pragma endregion Mode
 
-
+#pragma region PhLow
 void onShowSetPhLow(){
   lcd.clear();
   lcd.printstr("PhLow");
+
+  lcd.setCursor (0,1);
+  settings->phLow->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightPhLow(){
+  settings->phLow->inc();
+  onShowSetPhLow();
+}
+
+void onHoldLeftPhLow(){
+  settings->phLow->dec();
+  onShowSetPhLow();
+}
+#pragma endregion PhLow
+
+#pragma region PhHigh
 void onShowSetPhHigh(){
   lcd.clear();
   lcd.printstr("PhHigh");
+
+  lcd.setCursor (0,1);
+  settings->phHigh->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightPhHigh(){
+  settings->phHigh->inc();
+  onShowSetPhHigh();
+}
+
+void onHoldLeftPhHigh(){
+  settings->phHigh->dec();
+  onShowSetPhHigh();
+}
+#pragma endregion PhHigh
+
+#pragma region SmallAdjust
 void onShowSetSmallAdjust(){
   lcd.clear();
   lcd.printstr("Small Adjust");
+
+  lcd.setCursor (0,1);
+  settings->smallAdjust->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightSmallAdjust(){
+  settings->smallAdjust->inc();
+  onShowSetSmallAdjust();
+}
+
+void onHoldLeftSmallAdjust(){
+  settings->smallAdjust->dec();
+  onShowSetSmallAdjust();
+}
+#pragma endregion SmallAdjust
+
+#pragma region LargeAdjust
 void onShowSetLargeAdjust(){
   lcd.clear();
   lcd.printstr("Large Adjust");
+
+  lcd.setCursor (0,1);
+  settings->largeAdjust->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightLargeAdjust(){
+  settings->largeAdjust->inc();
+  onShowSetLargeAdjust();
+}
+
+void onHoldLeftLargeAdjust(){
+  settings->largeAdjust->dec();
+  onShowSetLargeAdjust();
+}
+#pragma endregion LargeAdjust
+
+#pragma region Delay
 void onShowSetDelay(){
   lcd.clear();
   lcd.printstr("Delay");
+
+  lcd.setCursor (0,1);
+  settings->delay->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightDelay(){
+  settings->delay->inc();
+  onShowSetDelay();
+}
+
+void onHoldLeftDelay(){
+  settings->delay->dec();
+  onShowSetDelay();
+}
+#pragma endregion Delay
+
+#pragma region LED
 void onShowSetLED(){
   lcd.clear();
   lcd.printstr("LED");
+
+  lcd.setCursor (0,1);
+  settings->LED->getDisplayValue(settingsStr);
+  lcd.printstr(settingsStr);
 }
 
+void onHoldRightLED(){
+  settings->LED->inc();
+  onShowSetLED();
+}
+
+void onHoldLeftLED(){
+  settings->LED->dec();
+  onShowSetLED();
+}
+#pragma endregion LED
+
+#pragma region ResetAll
 void onShowResetAll(){
   lcd.clear();
   lcd.printstr("Reset All");
 }
+#pragma endregion ResetAll
 #pragma endregion SettingsSubmenu
 
 #pragma region PhCalibrationSubmenu
@@ -174,12 +271,12 @@ Menu* createMenu(){
   // Create settings submenu
   Menu* settingsSubmenu = settings->subMenu;
   settingsSubmenu->add(new MenuElement(onShowSetMode, onHoldRightMode, onHoldLeftMode));
-  settingsSubmenu->add(new MenuElement(onShowSetPhLow));
-  settingsSubmenu->add(new MenuElement(onShowSetPhHigh));
-  settingsSubmenu->add(new MenuElement(onShowSetSmallAdjust));
-  settingsSubmenu->add(new MenuElement(onShowSetLargeAdjust));
-  settingsSubmenu->add(new MenuElement(onShowSetDelay));
-  settingsSubmenu->add(new MenuElement(onShowSetLED));
+  settingsSubmenu->add(new MenuElement(onShowSetPhLow, onHoldRightPhLow, onHoldLeftPhLow));
+  settingsSubmenu->add(new MenuElement(onShowSetPhHigh, onHoldRightPhHigh, onHoldLeftPhHigh));
+  settingsSubmenu->add(new MenuElement(onShowSetSmallAdjust, onHoldRightSmallAdjust, onHoldLeftSmallAdjust));
+  settingsSubmenu->add(new MenuElement(onShowSetLargeAdjust, onHoldRightLargeAdjust, onHoldLeftLargeAdjust));
+  settingsSubmenu->add(new MenuElement(onShowSetDelay, onHoldRightDelay, onHoldLeftDelay));
+  settingsSubmenu->add(new MenuElement(onShowSetLED, onHoldRightLED, onHoldLeftLED));
   settingsSubmenu->add(new MenuElement(onShowResetAll));
 
   // Create phCalibration submenu
